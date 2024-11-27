2e00864a9df63265f81c5970c11e44937d0b1ba9
aliso4ka <gulyayevaalisa@gmail.com>
2024-11-27 23:47:34 +0100

    mmm niced

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ const menu = document.querySelector('.mob-menu');
 const btnOrder = document.querySelector('.button-order');
 const modal = document.querySelector('.modal-overlay');
 const btnModalClose = document.querySelector('.button-modal');
+const mobNavLinks = document.querySelectorAll('.mob-navigation-item');
+const navLinks = document.querySelectorAll('.navigation-element');
+const subscribe = document.querySelector('.form-button');
+let lastScrollY = window.scrollY;
+const header = document.querySelector('.header');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').slice(1);
+    document.getElementById(targetId)?.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    navLinks.forEach(item => item.classList.remove('current'));
+    this.classList.add('current');
+  });
+});
 
 btnOpen.addEventListener('click', e => {
   e.preventDefault();
@@ -15,10 +35,44 @@ btnClose.addEventListener('click', e => {
   menu.classList.remove('is-open');
 });
 
+mobNavLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+  });
+});
+
 btnOrder.addEventListener('click', () => {
   modal.classList.add('is-open');
+  header.classList.add('under');
 });
 
 btnModalClose.addEventListener('click', () => {
   modal.classList.remove('is-open');
+  header.classList.remove('under');
+});
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY) {
+    header.classList.add('hidden');
+  } else {
+    header.classList.remove('hidden');
+  }
+
+  lastScrollY = currentScrollY;
+});
+
+subscribe.addEventListener('click', e => {
+  e.preventDefault();
+
+  const emailInput = document.querySelector('.email-class');
+
+  if (emailInput.value.trim() !== '') {
+    alert('Thank you for subscribing!');
+
+    emailInput.value = '';
+  } else {
+    alert('Please enter a valid email!');
+  }
 });
